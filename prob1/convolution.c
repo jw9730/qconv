ff55e6f510baa1cafe53745867e173e795ed8746
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-16 23:48:14 +0900

    debug

--- prob1/convolution.c
+++ prob1/convolution.c
@@ -25,8 +25,8 @@ float convolve(float * I, float * K, int n, int h, int w, int oc){
         for (int kh=0; kh<KH; kh++){
             for (int kw=0; kw<KW; kw++){
                 flag = (IH_L+kh < 0 || IH_L+kh >= H || IW_L+kw < 0 || IW_L+kw >= W);
+                printf("\t+= I[%d,%d/%d,%d/%d,%d] * K[%d,%d,%d,%d], out-of-bounds: %d \n", n, IH_L+kh, H-1, IW_L+kw, W-1, ic, kh, kw, oc, ic, flag);
                 if (flag){
-                    printf("\t+= I[%d,%d/%d,%d/%d,%d] * K[%d,%d,%d,%d], out-of-bounds: %d \n", n, IH_L+kh, H-1, IW_L+kw, W-1, ic, kh, kw, oc, ic, flag);
                     continue;
                 }
                 int input_idx = INDEX_ROW_MAJOR_4(n, IH_L+kh, IW_L+kw, ic, N, H, W, OC);
