312f11f5347da423114d95b66a7f76bf71be341b
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-17 00:20:54 +0900

    debug

--- prob2/run.sh
+++ prob2/run.sh
@@ -1,10 +1,10 @@
 make clean; make;
 ./convolution ../group1/1/input_tensor.bin ../group1/1/kernel_tensor.bin 32
-./convolution ../group1/2/input_tensor.bin ../group1/2/kernel_tensor.bin 32
-./convolution ../group1/3/input_tensor.bin ../group1/3/kernel_tensor.bin 32
 ./convolution ../group1/1/input_tensor.bin ../group1/1/kernel_tensor.bin 16
-./convolution ../group1/2/input_tensor.bin ../group1/2/kernel_tensor.bin 16
-./convolution ../group1/3/input_tensor.bin ../group1/3/kernel_tensor.bin 16
 ./convolution ../group1/1/input_tensor.bin ../group1/1/kernel_tensor.bin 8
+./convolution ../group1/2/input_tensor.bin ../group1/2/kernel_tensor.bin 32
+./convolution ../group1/2/input_tensor.bin ../group1/2/kernel_tensor.bin 16
 ./convolution ../group1/2/input_tensor.bin ../group1/2/kernel_tensor.bin 8
+./convolution ../group1/3/input_tensor.bin ../group1/3/kernel_tensor.bin 32
+./convolution ../group1/3/input_tensor.bin ../group1/3/kernel_tensor.bin 16
 ./convolution ../group1/3/input_tensor.bin ../group1/3/kernel_tensor.bin 8
