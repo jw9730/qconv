3887dac17d4b71e92da4bfe2c71f01c9001f692a
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-16 23:46:03 +0900

    debug

--- prob1/convolution.c
+++ prob1/convolution.c
@@ -26,7 +26,7 @@ float convolve(float * I, float * K, int n, int h, int w, int oc){
             for (int kw=0; kw<KW; kw++){
                 flag = (IH_L+kh < 0 || IH_L+kh >= H || IW_L+kw < 0 || IW_L+kw >= W);
                 if (flag){
-                    printf("\t+= I[%d,%d/%d,%d/%d,%d] * K[%d,%d,%d,%d], out-of-bounds: %d \n", n, IH_L+kh, H, IW_L+kw, W, ic, kh, kw, oc, ic, flag);
+                    printf("\t+= I[%d,%d/%d,%d/%d,%d] * K[%d,%d,%d,%d], out-of-bounds: %d \n", n, IH_L+kh, H-1, IW_L+kw, W-1, ic, kh, kw, oc, ic, flag);
                     continue;
                 }
                 int input_idx = INDEX_ROW_MAJOR_4(n, IH_L+kh, IW_L+kw, ic, N, H, W, OC);
@@ -119,8 +119,9 @@ int main(int argc, char **argv){
                 for (int oc=0; oc<OC; oc++){
                     // convolution for a single output pixel
                     int output_idx = INDEX_ROW_MAJOR_4(n, h, w, oc, N, H, W, OC);
+                    printf("main: O[%d,%d,%d,%d] = %0.3f\n", n, h, w, oc, O[output_idx]);
                     O[output_idx] = convolve(I, K, n, h, w, oc);
-                    // printf("main: O[%d,%d,%d,%d] = %0.3f\n", n, h, w, oc, O[output_idx]);
+                    printf("main: O[%d,%d,%d,%d] = %0.3f\n", n, h, w, oc, O[output_idx]);
                 }
             }
         }
