ca446bb78df824144cf26ea9078b3bb5646e4c0a
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-17 01:58:34 +0900

    debug

--- prob2/convolution.c
+++ prob2/convolution.c
@@ -104,12 +104,12 @@ void convolve_quantized(void * I_Q, void * K_Q, int n, int h, int w, int oc, enu
                 if (flag) continue;
                 int input_idx = INDEX_ROW_MAJOR_4(n, IH_L+kh, IW_L+kw, ic, N, H, W, C);
                 int kernel_idx = INDEX_ROW_MAJOR_4(kh, kw, oc, ic, KH, KW, OC, IC);
-                if (q == INT32) {
-                    ret32 += ((int32_t *) I_Q)[input_idx] * ((int32_t *) K_Q)[kernel_idx];
+                if (q == INT8){
+                    ret8 += ((int8_t *) I_Q)[input_idx] * ((int8_t *) K_Q)[kernel_idx];
                 } else if (q == INT16){
                     ret16 += ((int16_t *) I_Q)[input_idx] * ((int16_t *) K_Q)[kernel_idx];
-                } else if (q == INT8){
-                    ret8 += ((int8_t *) I_Q)[input_idx] * ((int8_t *) K_Q)[kernel_idx];
+                } else if (q == INT32) {
+                    ret32 += ((int32_t *) I_Q)[input_idx] * ((int32_t *) K_Q)[kernel_idx];
                 } else continue;
             }
         }
