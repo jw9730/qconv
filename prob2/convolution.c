94ce1c91c88518762069e67652accf9ced6cd8c0
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-17 11:59:16 +0900

    debug

--- prob2/convolution.c
+++ prob2/convolution.c
@@ -102,7 +102,7 @@ float convolve_quantized8(void * I_Q, void * K_Q, int n, int h, int w, int oc, f
                 int input_idx = INDEX_ROW_MAJOR_4(n, IH_L+kh, IW_L+kw, ic, N, H, W, C);
                 int kernel_idx = INDEX_ROW_MAJOR_4(kh, kw, oc, ic, KH, KW, OC, IC);
                 //printf("%f <- %d = %d * %d\n", ((float) (I[input_idx] * K[kernel_idx])) / scale2, I[input_idx] * K[kernel_idx], I[input_idx], K[kernel_idx]);
-                ret += ((float) I[input_idx] * K[kernel_idx]) / scale2;
+                ret += ((float) (I[input_idx] * K[kernel_idx])) / scale2;
             }
         }
     }
@@ -124,7 +124,7 @@ float convolve_quantized16(void * I_Q, void * K_Q, int n, int h, int w, int oc, 
                 int input_idx = INDEX_ROW_MAJOR_4(n, IH_L+kh, IW_L+kw, ic, N, H, W, C);
                 int kernel_idx = INDEX_ROW_MAJOR_4(kh, kw, oc, ic, KH, KW, OC, IC);
                 //printf("%f <- %d = %d * %d\n", ((float) (I[input_idx] * K[kernel_idx])) / scale2, I[input_idx] * K[kernel_idx], I[input_idx], K[kernel_idx]);
-                ret += ((float) I[input_idx] * K[kernel_idx]) / scale2;
+                ret += ((float) (I[input_idx] * K[kernel_idx])) / scale2;
             }
         }
     }
@@ -146,7 +146,7 @@ float convolve_quantized32(void * I_Q, void * K_Q, int n, int h, int w, int oc, 
                 int input_idx = INDEX_ROW_MAJOR_4(n, IH_L+kh, IW_L+kw, ic, N, H, W, C);
                 int kernel_idx = INDEX_ROW_MAJOR_4(kh, kw, oc, ic, KH, KW, OC, IC);
                 //printf("%f <- %d = %d * %d\n", ((float) (I[input_idx] * K[kernel_idx])) / scale2, I[input_idx] * K[kernel_idx], I[input_idx], K[kernel_idx]);
-                ret += ((float) I[input_idx] * K[kernel_idx]) / scale2;
+                ret += ((float) (I[input_idx] * K[kernel_idx])) / scale2;
             }
         }
     }
