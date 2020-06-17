feaaa3deaf5aacbfc67b33fd3054a07bec2e05a7
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-17 11:41:29 +0900

    debug

--- prob2/convolution.c
+++ prob2/convolution.c
@@ -134,7 +134,7 @@ float convolve_quantized(void * I_Q, void * K_Q, int n, int h, int w, int oc, en
                     if (flag) continue;
                     int input_idx = INDEX_ROW_MAJOR_4(n, IH_L+kh, IW_L+kw, ic, N, H, W, C);
                     int kernel_idx = INDEX_ROW_MAJOR_4(kh, kw, oc, ic, KH, KW, OC, IC);
-                    //printf("%f <- %d = %d * %d\n", ((float) (I[input_idx] * K[kernel_idx])) / scale2, I[input_idx] * K[kernel_idx], I[input_idx], K[kernel_idx]);
+                    printf("%f <- %d = %d * %d\n", ((float) (I[input_idx] * K[kernel_idx])) / scale2, I[input_idx] * K[kernel_idx], I[input_idx], K[kernel_idx]);
                     ret += ((float) I[input_idx] * K[kernel_idx]) / scale2;
                 }
             }
