b36d491aac01cc9055207bd220f2330028382afd
Jinwoo Kim <jinwoo-kim@kaist.ac.kr>
2020-06-18 17:07:40 +0900

    fix

--- test/run.sh
+++
@@ -1,3 +0,0 @@
-python3 __init__.py ../group1/1/input_tensor.bin ../group1/1/kernel_tensor.bin
-python3 __init__.py ../group1/2/input_tensor.bin ../group1/2/kernel_tensor.bin
-python3 __init__.py ../group1/3/input_tensor.bin ../group1/3/kernel_tensor.bin
